b0c75035e95f5f4390a679914d19fd4e150184db
Farzin mossaed <farzinmossaed@gmail.com>
2026-08-21 16:40:40 +0330

    add another component to export table data as zip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TableEditComponent} from './components/table-edit/table-edit.component';
+import {TableExportComponent} from './components/table-export/table-export.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableEditComponent],
+  imports: [RouterOutlet, TableEditComponent, TableExportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
